80c342c6bf6b7fce93cbbe4d1d4f2cb27ed29e78
SHREYAS JOSHI <85001364+okshreyas@users.noreply.github.com>
2023-05-07 00:30:18 +0530

    Memory Utilization Monitor (#7)
    +1 Added feature

--- Main.java
+++ Main.java
@@ -6,6 +6,15 @@ Class Main{
 		
 		apple=sc.nextInt();
 		System.out.println(apple);
+		Runtime runtime = Runtime.getRuntime();
+        
+        long usedMemory = runtime.totalMemory() - runtime.freeMemory();
+        long maxMemory = runtime.maxMemory();
+        double usedPercentage = (double) usedMemory / maxMemory * 100;
+        
+        System.out.println("Used memory: " + usedMemory + " bytes");
+        System.out.println("Max memory: " + maxMemory + " bytes");
+        System.out.println("Memory used percentage: " + usedPercentage + "%");
 	}
 
 }
